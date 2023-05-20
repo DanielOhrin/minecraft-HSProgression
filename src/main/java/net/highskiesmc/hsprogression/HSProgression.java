607a9c0bd89400cda9cbcdd38d5dd77dec414b47
Daniel Ohrin <xqiam@outlook.com>
2023-05-19 20:11:22 -0500

    Created IslandFishCaughtEvent

--- src/main/java/net/highskiesmc/hsprogression/HSProgression.java
+++ src/main/java/net/highskiesmc/hsprogression/HSProgression.java
@@ -1,17 +1,18 @@
 package net.highskiesmc.hsprogression;
 
+import net.highskiesmc.nodes.HSNodes;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin {
+public final class HSProgression extends JavaPlugin {
+    private final HSNodes hsNodes = HSNodes.getInstance();
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
