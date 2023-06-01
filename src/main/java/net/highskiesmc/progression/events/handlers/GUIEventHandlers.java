b5b16dd1513ea1bfd2332690d6d57ea4424f0fa5
Daniel Ohrin <xqiam@outlook.com>
2023-05-31 22:12:16 -0500

    Fixed typo

--- src/main/java/net/highskiesmc/progression/events/handlers/GUIEventHandlers.java
+++ src/main/java/net/highskiesmc/progression/events/handlers/GUIEventHandlers.java
@@ -150,7 +150,7 @@ public class GUIEventHandlers implements Listener {
                     // If it is NOT unlocked, AND the conditions are met for an upgrade
                     if (!ISLAND_DATA.getBoolean("unlocked") && ISLAND_DATA.getBoolean("conditions-met")) {
                         UpgradeGUI gui = this.createConfirmationInventory((Player) e.getWhoClicked(),
-                                IslandDataType.FARMING,
+                                IslandDataType.FISHING,
                                 this.API.getConfig(IslandDataType.FISHING).getDouble(fishType.getValue() + ".price"),
                                 null,
                                 null,
