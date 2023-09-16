ccaed286b5fd6a15afa9ee07a6b503c3c8c4e426
Daniel Ohrin <xqiam@outlook.com>
2023-09-16 14:08:54 -0500

    Added SuperiorSkyblock2 and createdIslandLevel cmd/enum

--- src/main/java/net/highskiesmc/progression/HSProgression.java
+++ src/main/java/net/highskiesmc/progression/HSProgression.java
@@ -1,5 +1,39 @@
 package net.highskiesmc.progression;
-// TODO: REVAMP *resources* directory
-public class HSProgression {
 
+import com.bgsoftware.superiorskyblock.api.SuperiorSkyblockAPI;
+import com.mattisadev.mcore.highskies.HSPlugin;
+import net.highskiesmc.progression.commands.superior.IslandUpgradeCommand;
+
+import javax.annotation.Nonnull;
+import java.util.Set;
+
+// TODO: REVAMP *resources* directory
+public class HSProgression extends HSPlugin {
+
+    @Override
+    public void enable() {
+        // Register SuperiorCommands
+        SuperiorSkyblockAPI.registerCommand(new IslandUpgradeCommand());
+    }
+
+    @Override
+    public void disable() {
+
+    }
+
+    @Override
+    public void reload() {
+
+    }
+
+    @Override
+    protected boolean isUsingInventories() {
+        return true;
+    }
+
+    @Nonnull
+    @Override
+    protected Set<String> getConfigFileNames() {
+        return Set.of("farming.yml", "fishing.yml", "slayer.yml", "mining.yml");
+    }
 }
